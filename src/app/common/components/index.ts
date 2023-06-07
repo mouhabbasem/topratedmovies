7964d66a312ac7e6b1a77ede1fd5bde24e374bc7
Mouhab Bassem <97340742+mouhabbasem@users.noreply.github.com>
2023-06-07 23:39:20 +0300

    export all

---
+++ src/app/common/components/index.ts
@@ -0,0 +1,7 @@
+import CardSkeleton from "./CardSkeleton";
+import Breadcrumb from "./Breadcrumb";
+import PageSkeleton from "./PageSkeleton";
+import TopBarSearch from "./TopBarSearch";
+import EmptyView from "./EmptyView";
+
+export { CardSkeleton, Breadcrumb, PageSkeleton, TopBarSearch, EmptyView };
