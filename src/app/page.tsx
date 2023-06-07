aa74b9d7496eb29c821b4ef1e306eee3f50fa083
Mouhab Bassem <97340742+mouhabbasem@users.noreply.github.com>
2023-06-08 00:15:04 +0300

    add movies list page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,16 @@
+import { SearchMovies } from "./modules/movies/components";
+import { MoviesList } from "./modules/movies/screens";
+
 export default function Home() {
-  return <p>Movies App</p>
+  return (
+    <div className="flex justify-center p-5">
+      <div className="w-full md:w-3/4">
+        {/* Render the search form */}
+        <SearchMovies />
+
+        {/* Render results */}
+        <MoviesList />
+      </div>
+    </div>
+  )
 }
