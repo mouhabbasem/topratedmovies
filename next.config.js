cceb70cc9eb79969e32564b58be5b9b76ee0d7ae
Mouhab Bassem <97340742+mouhabbasem@users.noreply.github.com>
2023-06-09 00:37:38 +0300

    revert config

--- next.config.js
+++ next.config.js
@@ -1,10 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: 'export',
-  experimental: {
-    runtime: "experimental-edge",
-    appDir: true,
-  },
-}
+const nextConfig = {}
 
 module.exports = nextConfig
