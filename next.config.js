ea515d71e14bfcc8d485462934519a0a30ba01e8
Mouhab Bassem <97340742+mouhabbasem@users.noreply.github.com>
2023-06-08 21:39:52 +0300

    trying deploy

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  basePath: '/topratedmovies'
+  basePath: '/topratedmovies',
+  output: 'export',
+  exportPathMap: async function () {
+    return {
+      '/': { page: '/' },
+      '/movies/[id]': { page: '/movies/[id]' },
+    };
+  },
 }
 
 module.exports = nextConfig
