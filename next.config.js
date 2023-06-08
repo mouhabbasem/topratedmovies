87b1910802c4b5166ffef09c9410a0c8dfa7511a
Mouhab Bassem <97340742+mouhabbasem@users.noreply.github.com>
2023-06-08 23:27:25 +0300

    Retry deploy

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  output: 'export',
+  experimental: {
+    runtime: "experimental-edge",
+    appDir: true,
+  },
+}
 
 module.exports = nextConfig
